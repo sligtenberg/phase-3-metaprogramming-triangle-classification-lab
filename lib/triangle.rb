15a1281fd09736dde4b81edbfd72cd516427803a
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-12 20:59:00 -0500

    commit tree b29aa1ff050232c886c0a8d56ee385dde5cfdd99

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    type = :scalene
+    if (@side1 == 0 || @side2 == 0 || @side3 == 0 || @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1)
+      raise TriangleError
+    elsif (@side1 == @side2 && @side1 == @side3)
+      type = :equilateral
+    elsif (@side1 == @side2 || @side1 == @side3 || @side2 == @side3)
+      type = :isosceles
+    end
+    type
+  end 
+
+  class TriangleError < StandardError
+  end
 end
